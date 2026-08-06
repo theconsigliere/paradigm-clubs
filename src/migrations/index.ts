1438a38cce784f07b89eab2b9be3d868f9e89011
theconsigliere <hello@maxwellkirwin.co.uk>
2026-08-06 21:24:04 +0100

    changes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,27 +1,9 @@
-import * as migration_20260804_213421_add_contact_section from './20260804_213421_add_contact_section';
-import * as migration_20260804_hero_type_cleanup from './20260804_hero_type_cleanup';
-import * as migration_20260806_000001_add_faq_block_content from './20260806_000001_add_faq_block_content';
-import * as migration_20260806_195304 from './20260806_195304';
+import * as migration_20260806_202225_baseline from './20260806_202225_baseline';
 
 export const migrations = [
   {
-    up: migration_20260804_213421_add_contact_section.up,
-    down: migration_20260804_213421_add_contact_section.down,
-    name: '20260804_213421_add_contact_section',
-  },
-  {
-    up: migration_20260804_hero_type_cleanup.up,
-    down: migration_20260804_hero_type_cleanup.down,
-    name: '20260804_hero_type_cleanup',
-  },
-  {
-    up: migration_20260806_000001_add_faq_block_content.up,
-    down: migration_20260806_000001_add_faq_block_content.down,
-    name: '20260806_000001_add_faq_block_content',
-  },
-  {
-    up: migration_20260806_195304.up,
-    down: migration_20260806_195304.down,
-    name: '20260806_195304'
+    up: migration_20260806_202225_baseline.up,
+    down: migration_20260806_202225_baseline.down,
+    name: '20260806_202225_baseline'
   },
 ];
