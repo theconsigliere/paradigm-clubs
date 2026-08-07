e1353a5c7d4559dd0d525b81dab0ee8d13bce041
theconsigliere <hello@maxwellkirwin.co.uk>
2026-08-07 15:34:20 +0100

    sort migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20260807_001309_add_new from './20260807_001309_add_new';
-import * as migration_20260807_142801 from './20260807_142801';
+import * as migration_20260807_142801 from './20260807_142801'
 
 export const migrations = [
   {
-    up: migration_20260807_001309_add_new.up,
-    down: migration_20260807_001309_add_new.down,
-    name: '20260807_001309_add_new',
-  },
-  {
     up: migration_20260807_142801.up,
     down: migration_20260807_142801.down,
-    name: '20260807_142801'
+    name: '20260807_142801',
   },
-];
+]
